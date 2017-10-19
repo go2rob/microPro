7dd2c9b7ab213ff0c8d82f5ad4df9648efe8e692
Robinson <robmail1996@gmail.com>
2017-10-19 19:57:49 +0530

    pg to proceed

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 import r from "../app.js";
 
+var assert = require('assert');
 var expect = require('chai').expect
+var should = require('chai').should();
 
-describe("test", function () {
-  it('should work as expected', function () {
-    expect(r).to.equal(8);
+
+describe("test", () => {
+  it('should work as expected', () => {
+    assert.equal(r,8);
   });
 });
+
+
+describe("now", () => {
+  it('check the expect if it works', () => {
+    // expect({a: 1}).to.has.key('a')
+    // expect({a: 1}).to.deep.equal({a: 1});
+    // expect({a: 1}).to.not.equal({a: 1});
+    expect([{}]).to.not.have.members([{}]);
+  });
+});
+
