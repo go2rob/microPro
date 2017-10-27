124f2ed901e2c7f98c8955407defdffc7d9a008b
Robinson <robmail1996@gmail.com>
2017-10-27 20:34:36 +0530

    traces updated

--- modules/app.js
+++ modules/app.js
@@ -1,12 +1,8 @@
-// import jquery from "../node_modules/jquery/dist/jquery.js"
-// import $ from "jquery"
 import index from "./index.js"
 System.import('jquery').then(function($) {
-  $(window).scrollTop(0);
-  index.test()
+  const app = () => {
+    index.test()
+  }
   
+  app.call(this)
 });
-const app = () => {
-
-}
-app.call(this)
